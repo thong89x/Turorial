ce0ceeb0dc6cc5967dd31fc64350d4187d836aef
thong89x <thong89x@gmail.com>
2022-11-11 22:08:27 +0700

    Thong: Back-end: Express Middle Ware and File Ejs

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,17 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.urlencoded({extended:true}))
 app.use(express.json());
-
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-});
+app.set('view engine','ejs')
 
 app.get('/',(req, res) => {
-    res.status(200).json('hello')
+    // res.status(200).json('hello')
+    res.render("index",{text: 'shot'});
 });
 const userRouter = require("./routes/users")
 app.use("/users",userRouter)
 
-
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
